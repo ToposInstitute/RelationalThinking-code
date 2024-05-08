d9562bd39a5e3c1a88e1ea40c848020b5923cd2e
pvarshinee <priyaa.mirra@gmail.com>
2024-05-08 17:22:33 +0300

    Fixed a few bugs

--- Ch5/Puzzle-2.jl
+++ Ch5/Puzzle-2.jl
@@ -1,25 +1,22 @@
-# Exercise 2
+# Puzzle 2
 #-----------
 
 using Catlab
 
-# Graph with a single isolated vertex
-Overlap1 = SymmetricGraph(1)
-# Create a triangle
-G2 = cycle_graph(SymmetricGraph, 3)
-# Initialize this graph as an isolated edge
-G3 = path_graph(SymmetricGraph, 2)
-# Then modify it to add a loop to vertex #2
-add_edge!(G3, 2, 2)
-
 # Our overlap is an isolated edge
 Overlap2 = path_graph(SymmetricGraph, 2)
+# Create a triangle
+graphA = cycle_graph(SymmetricGraph, 3)
+# Initialize this graph as an isolated edge
+graphB = path_graph(SymmetricGraph, 2)
+# Then modify it to add a loop to vertex #2
+add_edge!(graphB, 2, 2)
 
 # Again, the three possible morphisms out of Overlap2 (each of
 # which picks an edge of the triangle, G2) are equivalent, so
 # we don't need to pick a specific one: we let the automatic
 # search algorithm find it for us.
-G2_map = homomorphism(Overlap2, G2)
+graphA_map = homomorphism(Overlap2, graphA)
 
 # Again, we need to be more precise in how we map into G3
 # because it matters whether or not the overlapping edge is
@@ -28,9 +25,9 @@ G2_map = homomorphism(Overlap2, G2)
 # keyword). In this case, the morphism is fully determined
 # once we declare that both vertices of Overlap2 are sent
 # to vertex#2 in G3.
-G3_map = homomorphism(Overlap2, G3; initial=(V=[2, 2],))
+graphB_map = homomorphism(Overlap2, graphB; initial=(V=[2, 2],))
 
 # Once again we glue together G2 and G3 along Overlap2
-colim = colimit(Span(G2_map, G3_map));
+pushout_graph = colimit(Span(graphA_map, graphB_map));
 
-to_graphviz(apex(colim))
+to_graphviz(apex(pushout_graph))
